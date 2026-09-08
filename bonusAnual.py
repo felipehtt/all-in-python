adcb47d1588ba152ce7a09e05a349d03cec522a9
felipehtt <fs.oliveira0014@gmail.com>
2026-09-08 17:32:12 -0300

    exerc10

---
+++ bonusAnual.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import time
+
+os.system('cls')
+sys.stdout.write("\nCarregando programa...\n")
+time.sleep(2)
+os.system('cls')
+
+print("\nPrograma de cálculo de bônus anual.\n")
+
+salario = float(input("Informe o salário mensal: "))
+tempoCasa = float(input("Informe o tempo de casa (em anos): "))
+nota = float(input("Informe a nota do funcionário (de 0 a 10): "))
+
+os.system('cls')
+sys.stdout.write("\nCalculando bônus anual...\n")
+time.sleep(3)
+os.system('cls')
+
+if tempoCasa >= 1:
+    if nota >= 8:
+        bonus = salario * 0.2
+        print("\nParabéns! Você recebeu um bônus anual de R${:.2f}.\n".format(bonus))
+    else:
+        bonus = salario * 0.1
+        print("\nVocê recebeu um bônus anual de R${:.2f}.\n".format(bonus))
+else:
+    bonus = 0
+    print("\nVocê não tem direito a bônus anual.\n")
