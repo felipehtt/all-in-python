e6235a5b7fce09449c7be47b948e93afc3eeb395
felipehtt <fs.oliveira0014@gmail.com>
2026-09-08 14:56:09 -0300

    exerc2

---
+++ orcamento.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import time
+
+sys.stdout.write("\nCarregando programa...\n")
+time.sleep(2)
+os.system('cls')
+
+print("\nPrograma de verificação de orçamento.\n")
+monitor = float(input("Informe o valor do monitor: "))
+
+total = monitor * 3
+
+os.system('cls')
+
+print("\nVerificando se está dentro do orçamento...\n")
+
+time.sleep(2)
+
+if (total <= 2500):
+    print(True)
+else:
+    print(False)
