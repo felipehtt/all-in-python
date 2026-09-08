8fa087d506006ad5e45307af58929d25414d5164
felipehtt <fs.oliveira0014@gmail.com>
2026-09-08 14:17:04 -0300

    exerc1

---
+++ valorHora.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import time
+
+sys.stdout.write("\nCarregando programa...\n")
+
+time.sleep(2)
+
+os.system('cls')
+
+print("\nPrograma de calculo de valor por hora trabalhada.\n")
+
+quantDias = float(input("Informe quantos dias você trabalha por mês: "))
+horasDia = float(input("Informe quantas horas você trabalha por dia: "))
+salario = float(input("Informe quanto você recebe por mês: "))
+
+valorHora = salario / (horasDia * quantDias)
+
+time.sleep(0.5)
+
+os.system('cls')
+
+sys.stdout.write("\nCalculando valor por hora trabalhada...")
+
+time.sleep(3)
+
+os.system('cls')
+
+print("\nO valor que você recebe por hora trabalhada é de R$ {:.2f}\n".format(valorHora))
