d5080809df573cf44c631231c36685e250528f9f
felipehtt <fs.oliveira0014@gmail.com>
2026-09-22 15:00:21 -0300

    ex4

---
+++ intermediate/valida_login2.py
@@ -0,0 +1,28 @@
+import os, sys, time
+
+def validar_login(user, senha):
+    if user == "admin" and senha == "sys123":
+        print("\nAcesso permitido.\n")
+    else:
+        print("\nAcesso negado.\n")
+
+try:
+
+    os.system('cls')
+    sys.stdout.write("\nCarregando programa...\n")
+    time.sleep(2)
+    os.system('cls')
+
+    print("\nPrograma de validação de login.\n")
+    user = input("Informe o usuário: ")
+    senha = input("Informe a senha: ")
+
+    os.system('cls')
+    sys.stdout.write("\nValidando login...\n")
+    time.sleep(3)
+    os.system('cls')
+
+    validar_login(user, senha)
+
+except ValueError:
+    print("\nErro: Por favor, insira apenas valores válidos.\n")
