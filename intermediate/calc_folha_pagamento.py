db6b70a594c2844485eae77705154eec035ab6cc
felipehtt <fs.oliveira0014@gmail.com>
2026-09-22 15:13:54 -0300

    ex5

---
+++ intermediate/calc_folha_pagamento.py
@@ -0,0 +1,39 @@
+import os, sys, time
+
+def gerar_folha_pagamento(salario, beneficios):
+
+    inss = salario * 0.08
+    total = salario + beneficios - inss
+
+    if (total > 1412):
+        print("\nFolha de pagamento calculada com sucesso!\n")
+        print(f"Salário bruto: R$ {salario:.2f}")
+        print(f"Benefícios: R$ {beneficios:.2f}")
+        print(f"Desconto INSS: R$ {inss:.2f}")
+        print(f"Total a receber: R$ {total:.2f}\n")
+        
+    else:
+        print("\nHá um erro na folha de pagamento.\n")
+
+try:
+
+    os.system('cls')
+    sys.stdout.write("\nCarregando programa...\n")
+    time.sleep(2)
+    os.system('cls')
+
+    print("\nPrograma de cálculo de folha de pagamento.\n")
+
+    salario = float(input("Informe o salário bruto: "))
+    beneficios = float(input("Informe o valor dos benefícios: "))
+
+    os.system('cls')
+    sys.stdout.write("\nCalculando folha de pagamento...\n")
+    time.sleep(3)
+    os.system('cls')
+
+    gerar_folha_pagamento(salario, beneficios)
+
+except ValueError:
+
+    print("\nErro: Por favor, insira apenas valores válidos.\n")
